d32f6cd0f179b8e44bbe4a77f1e97627f665dfb7
peyro <peyrovskaaa@gmail.com>
2025-04-11 20:01:00 +0200

    Refactor error handling and improve logging in automation process

--- browser/automatization.py
+++ browser/automatization.py
@@ -11,13 +11,9 @@ BASE_URL = 'https://app.centraldispatch.com/'
 
 
 class Listing:
-
     _LISTING_URL = 'https://app.centraldispatch.com/listing-editor'
 
-
-    _DEFAULT_TEXT_VEHICLE_INFO = '''Please pick up the TITLE and Keys !Please take some PHOTOS around the car AT PICK UP and DROP OFF  send it to 954-703-4009 or  logisticsTauto@gmail.com !
-I’m paying on delivery by ZELLE'''
-
+    _DEFAULT_TEXT_VEHICLE_INFO = '''Please pick up the TITLE and Keys !Please take some PHOTOS around the car AT PICK UP and DROP OFF  send it to 954-703-4009 or  logisticsTauto@gmail.com !\nI’m paying on delivery by ZELLE'''
 
     def __init__(self, order: dict, local_storage: str, headless: bool = True):
         self.browser = Browser(headless=headless)
@@ -28,7 +24,7 @@ I’m paying on delivery by ZELLE'''
         terminals = ['SAVANNAH', 'NEW YORK', 'MIAMI']
         terminal = self.order.get('terminal')
         if terminal.upper() not in terminals:
-            raise CustomBadRequestWithDetail(f"invalid_terminal_in_order")
+            raise CustomBadRequestWithDetail("invalid_terminal_in_order")
 
         terminals = {
             'SAVANNAH': 'JAX AUTO SHIPPING INC NEW!!!',
@@ -39,7 +35,6 @@ I’m paying on delivery by ZELLE'''
 
     def get_location(self):
         location = self.order.get('auction_city').get('location')
-
         if location:
             location = ''.join(char for char in location if not char.isdigit())
             while '(' in location and ')' in location:
@@ -48,7 +43,7 @@ I’m paying on delivery by ZELLE'''
                 location = location[:start] + location[end:]
 
         if not location:
-            raise CustomBadRequestWithDetail(f"invalid_location_in_order")
+            raise CustomBadRequestWithDetail("invalid_location_in_order")
         return location.strip()
 
     def get_auction(self):
@@ -61,40 +56,41 @@ I’m paying on delivery by ZELLE'''
     def get_vin(self):
         vin = self.order.get('vin')
         if not vin:
-            raise CustomBadRequestWithDetail(f"invalid_vin_in_order")
+            raise CustomBadRequestWithDetail("invalid_vin_in_order")
         return vin
-
-
 
     def set_local_storage(self):
         while True:
             try:
+                print("[INFO] Навигация на страницу для установки local storage")
                 self.browser.navigate(self._LISTING_URL, False)
                 loader_selector = '.root-loader.root-interstitial-panel'
-                self.browser.page.wait_for_selector(
-                    loader_selector,
-                    state='visible',
-                    timeout=30000
-                )
+                print("[INFO] Ожидание загрузки лоадера")
+                self.browser.page.wait_for_selector(loader_selector, state='visible', timeout=30000)
+
+                print("[INFO] Включение offline режима и установка local storage")
                 self.browser.set_offline_mode(True)
                 self.browser.set_local_storage_from_string(fr'{self.loca_storage}')
                 self.browser.set_offline_mode(False)
+
+                print("[INFO] Повторная навигация после установки local storage")
                 self.browser.navigate(self._LISTING_URL)
                 break
             except TimeoutError:
+                print("[ERROR] Timeout при установке local storage")
                 return 'new_local_storage_needed'
 
     def fill_pickup_info(self):
         if self.browser.page.url != self._LISTING_URL:
+            print("[INFO] Навигация на страницу перед заполнением Pickup Info")
             self.browser.navigate(self._LISTING_URL)
         max_attempts = 3
         attempt = 0
 
         while attempt < max_attempts:
             try:
-                # Формируем исходную строку
                 original_string = f'{self.get_auction()} {self.get_location()}'.upper()
-
+                print(f"[DEBUG] Попытка №{attempt + 1}: ищем локацию {original_string}")
 
                 strings_to_try = [original_string]
                 if '-' in original_string:
@@ -105,187 +101,139 @@ I’m paying on delivery by ZELLE'''
                 for current_string in strings_to_try:
                     if success:
                         break
-
-
                     self.fill_string_field_by_id('pickup-location-name', '')
                     time.sleep(0.5)
-
 
                     for char in current_string:
                         self.fill_one_char_in_field_by_id('pickup-location-name', char)
                         time.sleep(random.uniform(0.2, 0.6))
-
                         try:
-
-                            self.browser.page.wait_for_selector('#ids-search-input-listbox-pickup-location-name',
-                                                                timeout=3000)
-
-
+                            self.browser.page.wait_for_selector('#ids-search-input-listbox-pickup-location-name', timeout=3000)
                             list_items = self.browser.page.locator('#ids-search-input-listbox-pickup-location-name li')
-                            if list_items.first.inner_text().upper() == current_string:
-                                # Если первый элемент совпадает с искомым, кликаем по нему
-                                time.sleep(0.4)
-                                list_items.first.click()
-                                success = True
-                                break
-                            item_count = list_items.count()
-                            if item_count == 1:
-                                time.sleep(0.4)
+                            if list_items.first.inner_text().upper() == current_string or list_items.count() == 1:
                                 list_items.first.click()
                                 success = True
                                 break
                         except Exception as e:
-
+                            print(f"[WARNING] Ошибка при выборе элемента: {e}")
                             continue
 
-
-                    if success:
-                        break
-
                 if success:
-                    # Если успешно выбрали локацию, переходим к выбору типа
-                    time.sleep(0.2)
+                    print("[INFO] Локация успешно выбрана, выбираем тип 'Auction'")
                     self.fill_string_field_by_id('ids-combo-box-input-pickup-location-type', 'Auction')
-                    time.sleep(0.2)
                     self.browser.page.wait_for_selector('#listbox-item-Auction')
-                    time.sleep(0.2)
                     self.browser.page.locator('#listbox-item-Auction').click()
                     return
                 else:
-                    # Если не удалось выбрать локацию, увеличиваем счетчик попыток
+                    print("[WARNING] Не удалось выбрать локацию. Повтор попытки...")
                     attempt += 1
                     time.sleep(1)
             except Exception as e:
+                print(f"[ERROR] Ошибка при попытке #{attempt + 1}: {e}")
                 attempt += 1
                 time.sleep(1)
 
-        if attempt >= max_attempts:
-            self.browser.close()
-            raise CustomBadRequestWithDetail('maximal_attempts_reached_while_filling_pickup_location')
+        print("[FATAL] Превышено максимальное количество попыток")
+        self.browser.close()
+        raise CustomBadRequestWithDetail('maximal_attempts_reached_while_filling_pickup_location')
 
     def fill_delivery_info(self):
+        print("[INFO] Заполнение Delivery Info")
         if self.browser.page.url != self._LISTING_URL:
             self.browser.navigate(self._LISTING_URL)
         self.fill_string_field_by_id('delivery-location-name', self.get_processed_terminal())
-        time.sleep(0.6)
         self.browser.page.wait_for_selector('#ids-search-input-listbox-delivery-location-name')
-        time.sleep(0.2)
         self.browser.page.locator('#ids-search-input-listbox-delivery-location-name li').first.click()
-        time.sleep(0.2)
         self.fill_string_field_by_id('ids-combo-box-input-delivery-location-type', 'Terminal')
-        time.sleep(0.2)
         self.browser.page.wait_for_selector('#listbox-item-Terminal')
-        time.sleep(0.2)
         self.browser.page.locator('#listbox-item-Terminal').click()
+        print("[INFO] Delivery Info заполнено")
 
     def fill_vehicle_info(self):
+        print("[INFO] Заполнение Vehicle Info")
         if self.browser.page.url != self._LISTING_URL:
             self.browser.navigate(self._LISTING_URL)
         self.browser.page.locator('[data-testid="listings_editor_vin_input_0"]').fill(self.get_vin())
-        time.sleep(0.2)
         self.browser.page.locator('[name="vehicle-detail-text-area-row"]').fill(self._DEFAULT_TEXT_VEHICLE_INFO)
-        time.sleep(0.2)
 
     def fill_delivery_details(self):
+        print("[INFO] Заполнение Delivery Details")
         if self.browser.page.url != self._LISTING_URL:
             self.browser.navigate(self._LISTING_URL)
         price = self.get_price_for_delivery()
-
-        date_pickup_field = self.browser.page.locator("#listings_editor_date-available-to-ship")
-        date_delivery_field = self.browser.page.locator("#listings_editor_desired-delivery-date")
+        print(f"[DEBUG] Получена цена доставки: {price}")
 
         current_date = datetime.now()
         future_date = current_date + timedelta(days=3)
-        current_date_str = current_date.strftime("%m/%d/%Y")
-        future_date_str = future_date.strftime("%m/%d/%Y")
-        date_pickup_field.fill(current_date_str)
-        time.sleep(0.2)
-        date_delivery_field.fill(future_date_str)
-        time.sleep(0.2)
+        self.browser.page.locator("#listings_editor_date-available-to-ship").fill(current_date.strftime("%m/%d/%Y"))
+        self.browser.page.locator("#listings_editor_desired-delivery-date").fill(future_date.strftime("%m/%d/%Y"))
         self.browser.page.locator('#amount-to-pay-carrier').fill(price)
-        time.sleep(0.2)
         self.browser.page.locator('#cod-amount').fill(price)
-        time.sleep(1)
-
         self.browser.page.locator('#cod-payment-method').click()
         self.browser.page.wait_for_selector('#listbox-item-Cash\\/CertifiedFunds')
-        self.browser.page.locator('#listbox-item-Cash\\/CertifiedFunds').first.click()
-        time.sleep(0.2)
+        self.browser.page.locator('#listbox-item-Cash\\/CertifiedFunds').click()
         self.browser.page.locator('#cod-location').click()
-        time.sleep(0.2)
         self.browser.page.wait_for_selector('#listbox-item-Delivery')
-        self.browser.page.locator('#listbox-item-Delivery').first.click()
-        time.sleep(0.2)
+        self.browser.page.locator('#listbox-item-Delivery').click()
 
     def fill_additional_info(self):
+        print("[INFO] Заполнение Additional Info")
         if self.browser.page.url != self._LISTING_URL:
             self.browser.navigate(self._LISTING_URL)
         self.browser.page.locator('#addtional-info-loadid').fill(f'{self.get_vin()[-6:]} TITLE MUST BE PICKED UP')
-        time.sleep(0.2)
         self.browser.page.locator('#addtional-info-terms').fill('Forklift on/off **Pick Up TITLE!** Zelle/wire payment')
-        time.sleep(0.2)
         self.browser.page.locator('.interstate-checkbox-input').click()
 
     def fill_all_fields(self):
+        print("[INFO] Начало заполнения всех полей")
         if self.browser.page.url != self._LISTING_URL:
             self.browser.navigate(self._LISTING_URL)
-
         self.fill_pickup_info()
         self.fill_delivery_info()
         self.fill_vehicle_info()
         self.fill_delivery_details()
         self.fill_additional_info()
+        print("[INFO] Все поля успешно заполнены")
 
     def get_screenshots(self):
         self.browser.page.evaluate("window.scrollTo(0, 0)")
         screenshot_path = 'pickup_location.png'
         self.browser.page.screenshot(path=screenshot_path)
         with open(screenshot_path, 'rb') as f:
-            image_data = f.read()
-        return image_data
+            return f.read()
 
     def delete_screenshots(self):
         if os.path.exists('pickup_location.png'):
             os.remove('pickup_location.png')
         self.browser.close()
 
-
-
     def post_listing(self):
+        print("[INFO] Публикация листинга")
         self.browser.page.locator('#post-listing-btn').click()
         time.sleep(2)
         self.browser.close()
+        print("[INFO] Листинг опубликован и браузер закрыт")
 
     def fill_string_field_by_id(self, field_id: str, value: str, delay=0.0):
         field = self.browser.page.locator(f'#{field_id}')
         if delay == 0.0:
             field.fill(value)
-            return
-        field.click()
-        for char in value:
-            field.type(char)
-            time.sleep(random.uniform(0.1, delay))
+        else:
+            field.click()
+            for char in value:
+                field.type(char)
+                time.sleep(random.uniform(0.1, delay))
 
     def fill_one_char_in_field_by_id(self, field_id: str, value: str):
         field = self.browser.page.locator(f'#{field_id}')
         field.click()
         field.type(value)
 
-    def fill_listing_info(self):
-        self.fill_string_field_by_id('pickup-location-name', self.vehicle['location'])
-
     def get_price_for_delivery(self):
+        print("[INFO] Получение цены доставки")
         self.browser.page.locator('.check-price-button').click()
         self.browser.page.wait_for_selector('table tbody tr')
-
-        first_data_row = self.browser.page.locator('table tbody tr').first
-
         self.browser.page.wait_for_selector('label:has-text("Status")')
-
-        column = first_data_row.locator('td.ant-table-cell.column-listed-price')
-        price = column.locator('div').first.inner_text().strip()
-
+        price = self.browser.page.locator('table tbody tr td.ant-table-cell.column-listed-price div').first.inner_text().strip()
         self.browser.page.locator('#ids-slideout-close-button').click()
-
         return price.replace('$', '')
-
--- browser/browser_control.py
+++ browser/browser_control.py
@@ -21,58 +21,65 @@ class Browser:
     ]
 
     def __init__(self, headless: bool = False):
+        print("[INFO] Инициализация браузера...")
         self.headless = headless
         self.user_agent = random.choice(self.USER_AGENTS)
         self.viewport = random.choice(self.VIEWPORT_SIZES)
+        print(f"[DEBUG] User-Agent: {self.user_agent}")
+        print(f"[DEBUG] Viewport: {self.viewport}")
+
         self.browser = sync_playwright().start().chromium.launch(headless=self.headless)
         self.context = self.browser.new_context(
             user_agent=self.user_agent,
             locale="en-US",
             timezone_id="UTC",
-
         )
+
         self.context.add_init_script(script="Object.defineProperty(navigator, 'webdriver', {get: () => false})")
         self.page = self.context.new_page()
 
         self.context.clear_cookies()
         self.context.clear_permissions()
+        print("[INFO] Браузер успешно запущен")
 
     def set_offline_mode(self, offline: bool = True):
         self.context.set_offline(offline)
+        print(f"[INFO] Установлен оффлайн режим: {offline}")
 
     def navigate(self, url: str, wait_until_full_load: bool = True):
         if self.page:
-            # Navigate to the URL
+            print(f"[INFO] Переход по ссылке: {url}")
             self.page.goto(url, wait_until='networkidle' if wait_until_full_load else None)
 
             if wait_until_full_load:
                 self.page.wait_for_load_state('networkidle')
 
             if self.page.url == url:
-                print("Page loaded successfully.")
+                print("[SUCCESS] Страница загружена успешно.")
                 if wait_until_full_load:
                     time.sleep(3)
                     content = self.page.content()
                     if "SIGN IN" in content:
+                        print("[ERROR] Обнаружено окно входа. Требуется обновление localStorage.")
                         self.browser.close()
                         raise CustomBadRequestWithDetail('update_local_storage')
             else:
-                print("Page failed to load.")
+                print("[ERROR] Не удалось загрузить страницу.")
                 self.browser.close()
                 raise CustomBadRequestWithDetail('update_local_storage')
 
-
-
     def get_cookies(self):
+        print("[INFO] Получение cookies из контекста")
         return self.context.cookies()
 
     def open_new_tab(self, url: str):
+        print(f"[INFO] Открытие новой вкладки с URL: {url}")
         self.page = self.context.new_page()
         self.page.goto(url)
 
     def set_local_storage_from_string(self, local_storage: str):
         data = json.loads(local_storage)
-        print(data)
+        print(f"[INFO] Установка localStorage: {data}")
         self.page.evaluate("""
             (data) => {
                 localStorage.clear();
@@ -83,28 +90,29 @@ class Browser:
             }
         """, data)
 
-
     def block_redirects(self):
+        print("[INFO] Блокировка редиректов включена")
         def intercept(route, request):
             if 300 <= request.response().status < 400:
                 route.abort()
             else:
                 route.continue_()
-
         self.page.route("**/*", intercept)
 
     def unblock_redirects(self):
+        print("[INFO] Блокировка редиректов отключена")
         self.page.unroute("**/*")
-
 
     def set_cookies_from_string(self, cookies: str):
         jsoned_cookies = json.loads(cookies)
+        print(f"[INFO] Установка cookies: {jsoned_cookies}")
         for cookie in jsoned_cookies:
             if "sameSite" in cookie:
                 cookie["sameSite"] = cookie["sameSite"].capitalize()
         self.context.add_cookies(jsoned_cookies)
 
     def close(self):
+        print("[INFO] Закрытие браузера...")
         if self.browser:
             self.browser.close()
-
+            print("[INFO] Браузер закрыт")
